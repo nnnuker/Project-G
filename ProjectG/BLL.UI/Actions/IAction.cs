baa84726feed20519d38fbf4098f17450e42ba6c
Valery Larkovich <valery_larkovich@epam.com>
2017-03-23 17:18:04 +0300

    Add BLL.UI project

---
+++ ProjectG/BLL.UI/Actions/IAction.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace BLL.UI.Actions
+{
+  /// <summary>
+  /// Represents mvc action.
+  /// </summary>
+  public interface IAction
+  {
+    /// <summary>
+    /// Processes the request asynchronous.
+    /// </summary>
+    /// <returns>Action result.</returns>
+    ActionResult ProcessAsync();
+  }
+}

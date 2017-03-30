c2ded0abc668eab9d755a537facc342f60c0e71c
Valery Larkovich <valery_larkovich@epam.com>
2017-03-30 17:42:47 +0300

    Fix filter and right panels

--- ProjectG/UI/App_Start/BundleConfig.cs
+++ ProjectG/UI/App_Start/BundleConfig.cs
@@ -8,10 +8,10 @@ namespace UI
     public static void RegisterBundles(BundleCollection bundles)
     {
       bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                  "~/Scripts/jquery-{version}.js"));
+                  "~/Scripts/JQuery/jquery-{version}.js"));
 
       bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                  "~/Scripts/jquery.validate*"));
+                  "~/Scripts/JQuery/jquery.validate*"));
 
       // Use the development version of Modernizr to develop with and learn from. Then, when you're
       // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -23,14 +23,10 @@ namespace UI
                 "~/Scripts/respond.js"));
 
       bundles.Add(new ScriptBundle("~/bundles/angular").Include(
-                "~/Scripts/angular.min.js",
+                "~/Scripts/Angular/angular.min.js",
                 "~/Scripts/layout.js",
                 "~/Scripts/Controllers/FilterPanelController.js",
                 "~/Scripts/Controllers/RightPanelController.js"));
-
-      bundles.Add(new ScriptBundle("~/bundles/siteScripts").Include(
-                "~/Scripts/layout.js",
-                "~/Scripts/bundle.js"));
 
       bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
